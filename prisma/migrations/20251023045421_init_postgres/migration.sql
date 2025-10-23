54e5a5ea29bba5cab51ae9db2d4de410e835246c
0xnull <74732963+excelliumCoin@users.noreply.github.com>
2025-10-23 04:57:08 +0000

    local: api route fix + logo glow

--- prisma/migrations/20251023045421_init_postgres/migration.sql
+++ prisma/migrations/20251023045421_init_postgres/migration.sql
@@ -1,17 +1,19 @@
 -- CreateTable
 CREATE TABLE "Submission" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "taskId" INTEGER NOT NULL,
     "user" TEXT NOT NULL,
     "action" INTEGER NOT NULL,
     "proofUrl" TEXT NOT NULL,
     "note" TEXT,
     "status" TEXT NOT NULL DEFAULT 'pending',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "approvedAt" DATETIME,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "approvedAt" TIMESTAMP(3),
     "approvedBy" TEXT,
     "amountWei" TEXT,
     "nonce" TEXT,
     "deadline" TEXT,
-    "signature" TEXT
+    "signature" TEXT,
+
+    CONSTRAINT "Submission_pkey" PRIMARY KEY ("id")
 );
